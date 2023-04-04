e523b654696580d5818a0b2c2d0f7fc2070f21e3
psnluiz <psnluiz@hotmail.com>
2023-04-04 03:39:24 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Luiz Fernando Andrade on 04/04/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
